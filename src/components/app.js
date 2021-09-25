50c46ad72c32513531b7a0f7d36e4ed0f3c8141a
zearkiatos <caprilespe@outlook.com>
2021-09-25 20:24:45 -0300

    React integration with 3rd party libraries

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import GoogleMap from "./GoogleMap";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: -34.397, lng: 150.644 };
+    this.state = { latitude: -34.397, longitud: 150.644 };
   }
 
   render() {
     return (
-      <div style={{height: '100%'}}>
+      <div style={{ height: "100%" }}>
         Map me!
+        <button
+          onClick={() =>
+            this.setState({ latitude: -34.397, longitud: 150.644 })
+          }
+        >
+          New York
+        </button>
+        <GoogleMap
+          longitud={this.state.longitud}
+          latitude={this.state.latitude}
+        />
       </div>
     );
   }
