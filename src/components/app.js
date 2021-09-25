e790a8702e08c66ee69d44a105a70603894d6478
zearkiatos <caprilespe@outlook.com>
2021-09-25 19:19:40 -0300

    Base project

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import GoogleMap from './GoogleMap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: -34.397, lng: 150.644 };
+  }
+
+  render() {
+    return (
+      <div style={{height: '100%'}}>
+        Map me!
+        <button onClick={() => this.setState({ lat: 40.7128, lng: -74.0059 })}>
+          New York
+        </button>
+        <GoogleMap lat={this.state.lat} lng={this.state.lng} />
+      </div>
+    );
+  }
+}
+
+export default App;
